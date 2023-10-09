99791ead38378cfb2c08bd2dba8974543a94d4aa
haha4un <savelijstulev3872@gmail.com>
2023-10-09 19:57:48 +0500

    4

--- app/src/main/java/com/example/translator_fwathces/presentation/MainActivity.kt
+++ app/src/main/java/com/example/translator_fwathces/presentation/MainActivity.kt
@@ -8,8 +8,6 @@ package com.example.translator_fwathces.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import android.speech.RecognizerIntent
-import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.View
 import android.webkit.URLUtil
@@ -55,16 +53,27 @@ import androidx.wear.compose.material.Text
 import com.example.translator_fwathces.R
 import com.example.translator_fwathces.presentation.theme.Translator_fwathcesTheme
 import java.util.Locale
+import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
-class MainActivity : ComponentActivity() {
+
+const val API_KEY = "cf";
+class MainActivity : ComponentActivity(), TextToSpeech.OnInitListener {
     private var tts: TextToSpeech? = null
-    private var orgtext: EditText? = null
+    private var orgtext: String? = ""
+    private var trstext: String? = "";
     private var tospeak: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WearApp("Android")
         }
+        tts = TextToSpeech(this, this)
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, @Nullable data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -72,15 +81,45 @@ class MainActivity : ComponentActivity() {
             when (requestCode) {
                 10 -> {
                     val text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-                    var pre_text = "${orgtext!!.text.toString()}"
+                    var pre_text = "${orgtext}"
                     pre_text+=text!![0] +".  "
-                    orgtext!!.setText(pre_text)
+                    orgtext = pre_text
                 }
             }
         }
     }
+//    fun getdata(){
+//        val url = "$API_KEY"
+//        val queue = Volley.newRequestQueue(this)
+//        val request = StringRequest(Request.Method.GET, url,
+//            {result ->Toast.makeText(this, "it resulted! $result", Toast.LENGTH_SHORT).show()},
+//            {error -> Toast.makeText(this, "it erred! $error", Toast.LENGTH_SHORT).show()}
+//        )
+//        queue.add(request)
+//    }
+//    fun parsedata(result: String){
+//        val obj = JSONObject(result)
+//    }
+    override fun onInit(status: Int) {
+
+        if (status == TextToSpeech.SUCCESS) {
+            // set US English as language for tts
+            val result = tts!!.setLanguage(Locale.ROOT)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS","The Language specified is not supported!")
+            } else {
+                tospeak!!.isEnabled = true
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!")
+        }
+
+    }
+
     private fun speakOut() {
-        val text = orgtext!!.text.toString()
+        val text = orgtext
         tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null,"")
     }
 
@@ -93,40 +132,48 @@ class MainActivity : ComponentActivity() {
         super.onDestroy()
     }
 
-}
 
-@Composable
-fun WearApp(greetingName: String) {
-    Translator_fwathcesTheme {
+    @Composable
+    fun WearApp(greetingName: String) {
+        Translator_fwathcesTheme {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colors.background),
+                verticalArrangement = Arrangement.Center
+            ) {
+                maingtid()
+            }
+        }
+    }
+    fun speakin(){
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        startActivityForResult(intent, 10)
+    }
+
+    @Composable
+    fun maingtid() {
+        var org_t by remember {
+            mutableStateOf(orgtext)
+        }
+        var trs_t by remember {
+            mutableStateOf(trstext)
+        }
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(MaterialTheme.colors.background),
-            verticalArrangement = Arrangement.Center
-        ) {
-            maingtid()
-        }
-    }
-}
-
-@Composable
-fun maingtid() {
-    var org_t by remember {
-        mutableStateOf("")
-    }
-    var trs_t by remember {
-        mutableStateOf("")
-    }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFFF7F7F7))){
-        //
+                .background(Color(0xFFF7F7F7))){
+            //
             Button(modifier = Modifier
                 .fillMaxWidth()
                 .height(30.dp)
                 .background(color = Color(0xFF2F2F2F)),
-                onClick = { /*TODO*/ }) {
+                onClick = {speakin()}) {
                 Text(text = "\uD83C\uDFA4",)
             }
 
@@ -138,8 +185,8 @@ fun maingtid() {
                 .align(alignment = Alignment.CenterHorizontally)
                 .border(width = 3.dp, color = Color(0xFF8D8D8D))
                 .background(color = Color(0xFFF7F7F7)),
-                value = org_t, onValueChange = {text -> org_t = text}, textStyle = TextStyle(
-                    color = Color.White,
+                value = org_t!!, onValueChange = {text -> org_t = text}, textStyle = TextStyle(
+                    color = Color(0xFF2F2F2F),
                     textAlign = TextAlign.Center))
 
             Row(modifier = Modifier
@@ -156,23 +203,21 @@ fun maingtid() {
                     text = "->",
                     style = TextStyle(
                         fontSize = 16.sp,
-                        color = Color(0xFFF7F7F7),))
+                        color = Color(0xFF2F2F2F),))
                 Text(
                     text = "🇬🇧",
                     style = TextStyle(
                         fontSize = 16.sp,
                         color = Color(0xFF000000),))
             }
-
-            BasicTextField(modifier = Modifier
+            Text(modifier = Modifier
                 .fillMaxWidth()
                 .height(50.dp)
                 .align(alignment = Alignment.CenterHorizontally)
                 .border(width = 3.dp, color = Color(0xFF8D8D8D))
                 .background(color = Color(0xFFF7F7F7), shape = RoundedCornerShape(size = 0.dp)),
-                value = org_t, onValueChange = {text -> org_t = text}, textStyle = TextStyle(
-                    color = Color.White,
-                    textAlign = TextAlign.Center))
+            text = trs_t!!
+            )
 
             Spacer(modifier = Modifier.height(5.dp))
 
@@ -180,16 +225,19 @@ fun maingtid() {
                 .fillMaxWidth()
                 .height(30.dp)
                 .background(color = Color(0xFF2F2F2F)),
-                onClick = { /*TODO*/ }) {
+                onClick = { speakOut() }) {
                 Text(text = "\uD83D\uDD0A",)
             }
-        //
+            //
+        }
+    }
+
+    @Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
+    @Composable
+    fun DefaultPreview() {
+        WearApp("Preview Android")
     }
 }
 
-@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    WearApp("Preview Android")
-}
+
 
